a446204efaa58f45e05d3274091e83ad835118f7
Kartik8Dwivedi <codewithme.kartik@gmail.com>
2023-12-23 01:04:12 +0530

    codebase updated

---
+++ src/config/api.js
@@ -0,0 +1,7 @@
+const API_DOMAIN = "https://gnews.io/api/v4/top-headlines?country=";
+const API_SEARCH_DOMAIN = "https://gnews.io/api/v4/search?q=";
+const API_KEY = "ddf9aa057c12ed04b772acb0f9082413";
+export const endpointPath = (country, category) =>
+  `${API_DOMAIN}${country}&lang=en&category=${category}&apikey=${API_KEY}`;
+export const endpointSearch = (searchQuery) =>
+  `${API_SEARCH_DOMAIN}${searchQuery}&lang=en&apikey=${API_KEY}`;
